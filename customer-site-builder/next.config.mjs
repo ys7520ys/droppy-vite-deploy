12514119be2b1562b71bab1b239fb49dce15a73e
yeseong Kim <ys7520ys@naver.com>
2025-05-11 22:10:49 +0900

    fourth commit

--- customer-site-builder/next.config.mjs
+++ customer-site-builder/next.config.mjs
@@ -18,12 +18,27 @@
 
 
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     unoptimized: true,  // ✅ 이미지 최적화 비활성화!
+//   },
+// };
+
+// export default nextConfig;
+
+
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true,  // ✅ 이미지 최적화 비활성화!
+    unoptimized: true,
   },
+  output: 'export', // ✅ 정적 사이트로 export 명시
 };
 
 export default nextConfig;
